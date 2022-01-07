088221cb38bde060384804649d457b5b926cbb0b
matheussribeiro <mtsales1337@gmail.com>
2022-01-07 16:53:14 -0300

    implement interface

---
+++ BackEnd/src/ProEventos.Persistence/ProEventosPersistence.cs
@@ -0,0 +1,69 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public class ProEventosPersistence : IProEventosPersistence
+    {
+        private readonly ProEventosContext _context;
+        public ProEventosPersistence(ProEventosContext context)
+        {
+            _context = context;
+
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public void DeleteRange<T>(T entity) where T : class
+        {
+            _context.RemoveRange(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        public Task<Evento[]> GetAllEventosAsync(bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Evento> GetAllEventosByIdAsync(int eventoId, bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Palestrante> GetAllPalestrantesByIdAsync(int palestranteId, bool includeEventos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
