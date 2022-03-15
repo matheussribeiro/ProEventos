9396eb65b852bb9cfba67a836ef5c1b986f929f4
matheussribeiro <mtsales1337@gmail.com>
2022-03-15 15:46:20 -0300

    Finish Seção 7

--- FrontEnd/ProEventos-App/src/app/app-routing.module.ts
+++ FrontEnd/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eventos', component:EventosComponent},
+  {path: '', redirectTo:'dashboard', pathMatch:'full'},
+  {path: 'palestrantes', component:PalestrantesComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'perfil', component:PerfilComponent},
+  {path: 'contatos', component:ContatosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/ProEventos-App/src/app/app.module.ts
+++ FrontEnd/ProEventos-App/src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
-import { EventoService } from './services/evento.service';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ToastrModule } from 'ngx-toastr';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { EventoService } from './services/evento.service';
+import { TitleComponent } from './shared/title/title.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-    NavComponent
+    NavComponent,
+    TitleComponent,
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent
    ],
   imports: [
     BrowserModule,
@@ -41,6 +51,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     NgxSpinnerModule
   ],
   providers: [EventoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
